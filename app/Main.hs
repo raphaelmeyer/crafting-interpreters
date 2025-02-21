35654614e4c96a2a8a3da2e15c23949be1e52b07
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-02-21 07:48:58 +0100

    (4.1) add readfile and prompt

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified Control.Monad as M
+import Data.Text.IO as Text.IO
+import qualified HLox
+import qualified System.Environment as Environment
+import qualified System.Exit as Exit
+import qualified System.IO
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  args <- Environment.getArgs
+  case args of
+    [f] -> runFile f
+    [] -> runPrompt
+    _ -> usage
+
+usage :: IO ()
+usage = do
+  name <- Environment.getProgName
+  System.IO.hPutStrLn System.IO.stderr $ "Usage: " ++ name ++ " [script]"
+  Exit.exitFailure
+
+runFile :: String -> IO ()
+runFile f = do
+  script <- Text.IO.readFile f
+  HLox.run script
+
+runPrompt :: IO ()
+runPrompt = do
+  Text.IO.putStr "> "
+  System.IO.hFlush System.IO.stdout
+  done <- System.IO.isEOF
+  M.unless done $ do
+    line <- Text.IO.getLine
+    HLox.run line
+    runPrompt
---
+++ src/HLox.hs
@@ -0,0 +1,6 @@
+module HLox (run) where
+
+import qualified Data.Text as Text
+
+run :: Text.Text -> IO ()
+run = undefined
