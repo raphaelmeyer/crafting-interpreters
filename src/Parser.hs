6360e37bd61325f0131798ccdc20ae1017fa5694
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-03-17 21:24:38 +0100

    (6.2) remaining binary operators

--- src/Parser.hs
+++ src/Parser.hs
@@ -42,13 +42,66 @@ equalityOp t
   | otherwise = Nothing
 
 comparison :: State.State Parser Expr.Expr
-comparison = term
+comparison = do
+  expr <- term
+  whileComparison expr
+
+whileComparison :: Expr.Expr -> State.State Parser Expr.Expr
+whileComparison expr = do
+  maybeOp <- State.state $ match comparisonOp
+  case maybeOp of
+    Just op -> do
+      right <- term
+      whileComparison $ Expr.Binary expr right op
+    Nothing -> pure expr
+
+comparisonOp :: Token.Token -> Maybe Expr.BinaryOp
+comparisonOp t
+  | Token.tType t == Token.Greater = Just Expr.Greater
+  | Token.tType t == Token.GreaterEqual = Just Expr.GreaterEqual
+  | Token.tType t == Token.Less = Just Expr.Less
+  | Token.tType t == Token.LessEqual = Just Expr.LessEqual
+  | otherwise = Nothing
 
 term :: State.State Parser Expr.Expr
-term = factor
+term = do
+  expr <- factor
+  whileTerm expr
+
+whileTerm :: Expr.Expr -> State.State Parser Expr.Expr
+whileTerm expr = do
+  maybeOp <- State.state $ match termOp
+  case maybeOp of
+    Just op -> do
+      right <- factor
+      whileTerm $ Expr.Binary expr right op
+    Nothing -> pure expr
+
+termOp :: Token.Token -> Maybe Expr.BinaryOp
+termOp t
+  | Token.tType t == Token.Minus = Just Expr.Minus
+  | Token.tType t == Token.Plus = Just Expr.Plus
+  | otherwise = Nothing
 
 factor :: State.State Parser Expr.Expr
-factor = unary
+factor = do
+  expr <- unary
+  whileFactor expr
+
+whileFactor :: Expr.Expr -> State.State Parser Expr.Expr
+whileFactor expr = do
+  maybeOp <- State.state $ match factorOp
+  case maybeOp of
+    Just op -> do
+      right <- unary
+      whileFactor $ Expr.Binary expr right op
+    Nothing -> pure expr
+
+factorOp :: Token.Token -> Maybe Expr.BinaryOp
+factorOp t
+  | Token.tType t == Token.Slash = Just Expr.Div
+  | Token.tType t == Token.Star = Just Expr.Mult
+  | otherwise = Nothing
 
 unary :: State.State Parser Expr.Expr
 unary = primary
