f6499cafff3a41b9d541dff631a180df10bb05fe
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-03-23 08:09:44 +0100

    (7.2) evaluate binary expressions

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,5 +1,6 @@
 module Interpreter (interpret) where
 
+import qualified Data.Text as Text
 import qualified Expr
 import qualified Lox
 
@@ -10,12 +11,26 @@ evaluate :: Expr.Expr -> Lox.Value
 evaluate (Expr.Literal l) = l
 evaluate (Expr.Grouping g) = evaluate g
 evaluate (Expr.Unary r op) = evalUnary op . evaluate $ r
-evaluate _ = undefined
+evaluate (Expr.Binary l r op) = evalBinary op (evaluate l) (evaluate r)
 
 evalUnary :: Expr.UnaryOp -> Lox.Value -> Lox.Value
 evalUnary Expr.Neg (Lox.Number n) = Lox.Number (-n)
 evalUnary Expr.Not v = Lox.Boolean . not . truthy $ v
 evalUnary _ _ = Lox.Nil
+
+evalBinary :: Expr.BinaryOp -> Lox.Value -> Lox.Value -> Lox.Value
+evalBinary Expr.Minus (Lox.Number a) (Lox.Number b) = Lox.Number (a - b)
+evalBinary Expr.Mult (Lox.Number a) (Lox.Number b) = Lox.Number (a * b)
+evalBinary Expr.Div (Lox.Number a) (Lox.Number b) = Lox.Number (a / b)
+evalBinary Expr.Plus (Lox.Number a) (Lox.Number b) = Lox.Number (a + b)
+evalBinary Expr.Plus (Lox.String a) (Lox.String b) = Lox.String (Text.append a b)
+evalBinary Expr.Greater (Lox.Number a) (Lox.Number b) = Lox.Boolean (a > b)
+evalBinary Expr.GreaterEqual (Lox.Number a) (Lox.Number b) = Lox.Boolean (a >= b)
+evalBinary Expr.Less (Lox.Number a) (Lox.Number b) = Lox.Boolean (a < b)
+evalBinary Expr.LessEqual (Lox.Number a) (Lox.Number b) = Lox.Boolean (a <= b)
+evalBinary Expr.Equal a b = Lox.Boolean (a == b)
+evalBinary Expr.NotEqual a b = Lox.Boolean (a /= b)
+evalBinary _ _ _ = Lox.Nil
 
 truthy :: Lox.Value -> Bool
 truthy Lox.Nil = False
