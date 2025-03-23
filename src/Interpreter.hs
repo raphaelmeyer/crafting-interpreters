c56e9849dc939cd26f967f66ea9621a67408c24b
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-03-23 07:51:12 +0100

    (7.2) evaluate unary expressions

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -9,4 +9,15 @@ interpret = evaluate
 evaluate :: Expr.Expr -> Lox.Value
 evaluate (Expr.Literal l) = l
 evaluate (Expr.Grouping g) = evaluate g
+evaluate (Expr.Unary r op) = evalUnary op . evaluate $ r
 evaluate _ = undefined
+
+evalUnary :: Expr.UnaryOp -> Lox.Value -> Lox.Value
+evalUnary Expr.Neg (Lox.Number n) = Lox.Number (-n)
+evalUnary Expr.Not v = Lox.Boolean . not . truthy $ v
+evalUnary _ _ = Lox.Nil
+
+truthy :: Lox.Value -> Bool
+truthy Lox.Nil = False
+truthy (Lox.Boolean b) = b
+truthy _ = True
