25cbda02f238e33a1931da9adfbf0f5ff838dc30
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-03-24 21:15:36 +0100

    (7) format error message

--- src/Error.hs
+++ src/Error.hs
@@ -6,4 +6,10 @@ data Error = Error
   { eLine :: Int,
     eMessage :: Text.Text
   }
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show Error where
+  show e = "[Line " ++ line ++ "] Error: " ++ message
+    where
+      line = show . eLine $ e
+      message = Text.unpack . eMessage $ e
