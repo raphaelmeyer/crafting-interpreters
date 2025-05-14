e7e3beb67599d28eaffa85203a2720767a78d9d2
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-05-14 23:10:45 +0200

    (14.3) define chunk

--- alox/src/alox.adb
+++ alox/src/alox.adb
@@ -1,4 +1,14 @@
-procedure Alox is
+with Ada.Text_IO;
+with Chunk;
+
+procedure ALox is
+   Test : Chunk.Chunk;
 begin
-   null;
-end Alox;
+   Ada.Text_IO.Put_Line ("alox");
+   Chunk.Init (Test);
+   Chunk.Write (Test, Chunk.Op_Return);
+
+   Ada.Text_IO.Put_Line (Chunk.Op_Code'Image (Test.Code (0)));
+
+   Chunk.Free (Test);
+end ALox;
