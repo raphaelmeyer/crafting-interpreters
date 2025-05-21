28c6dcb3e14b4eaf97db47b96ef50b976b21cba3
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-05-21 22:24:19 +0200

    (14.5) allow to write raw bytes to chunks

--- alox/src/chunk.ads
+++ alox/src/chunk.ads
@@ -2,20 +2,22 @@ with Ada.Finalization;
 with Value;
 
 package Chunk is
+   type Byte is mod 2**8 with Size => 8;
    type Op_Code is (Op_Constant, Op_Return) with Size => 8;
 
-   type Code_Array is array (Natural range <>) of Op_Code;
-   type Code_Array_Access is access all Code_Array;
+   type Byte_Array is array (Natural range <>) of Byte;
+   type Byte_Array_Access is access all Byte_Array;
 
    type Chunk is new Ada.Finalization.Controlled with record
       Count     : Natural;
       Capacity  : Natural;
-      Code      : Code_Array_Access;
+      Code      : Byte_Array_Access;
       Constants : Value.Value_Array;
    end record;
 
    procedure Init (C : in out Chunk);
-   procedure Write (C : in out Chunk; B : Op_Code);
+   procedure Write (C : in out Chunk; B : Byte);
+   procedure Write (C : in out Chunk; O : Op_Code);
    procedure Free (C : in out Chunk);
 
    function Add_Constant (C : in out Chunk; V : Value.Value) return Natural;
