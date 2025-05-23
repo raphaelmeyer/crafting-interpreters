d237834de63aa7c7ff31b93a6c3f8ff89608992d
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-05-23 22:06:30 +0200

    [ada] (14) clean up disassemble output formatting

--- alox/tests/src/value_tests.adb
+++ alox/tests/src/value_tests.adb
@@ -67,6 +67,24 @@ package body Value_Tests is
       Assert (Testee.Values = null, "Should not have data");
    end Test_Free;
 
+   procedure Test_To_String (T : in out AUnit.Test_Cases.Test_Case'Class) is
+      procedure Assert_String_Eq (Actual : String; Expected : String) is
+      begin
+         Assert
+           (Actual = Expected,
+            "Expected: " & Expected & ", Actual: " & Actual);
+      end Assert_String_Eq;
+   begin
+      Assert_String_Eq (Value.To_String (23.0), "23");
+      Assert_String_Eq (Value.To_String (2.3), "2.3");
+      Assert_String_Eq (Value.To_String (0.023), "0.023");
+      Assert_String_Eq (Value.To_String (1234567.8), "1.23457E+06");
+      Assert_String_Eq (Value.To_String (-456789123.456), "-4.56789E+08");
+      Assert_String_Eq (Value.To_String (0.0000000123), "1.23000E-08");
+      Assert_String_Eq (Value.To_String (-0.00000004567), "-4.56700E-08");
+      Assert_String_Eq (Value.To_String (0.0), "0");
+   end Test_To_String;
+
    overriding
    procedure Register_Tests (T : in out Value_Test) is
       use AUnit.Test_Cases.Registration;
@@ -75,6 +93,7 @@ package body Value_Tests is
       Register_Routine (T, Test_Append'Access, "Append");
       Register_Routine (T, Test_Grow'Access, "Grow");
       Register_Routine (T, Test_Free'Access, "Free");
+      Register_Routine (T, Test_To_String'Access, "To String");
    end Register_Tests;
 
    overriding
