5d8d791226a9c8bfae48678cd8e2dd5aef5b17a4
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-06-04 19:52:37 +0200

    (11.3) resolve variable declarations

--- src/Resolver/Resolver.hs
+++ src/Resolver/Resolver.hs
@@ -4,6 +4,7 @@ import qualified Control.Monad.State.Strict as State
 import qualified Data.Map.Strict as Map
 import qualified Data.Text as Text
 import qualified Lox
+import qualified Parser.Expr as Expr
 import qualified Parser.Stmt as Stmt
 
 type Scope = Map.Map Text.Text Bool
@@ -26,7 +27,29 @@ statement (Stmt.Block block) = do
   resolved <- program block
   endScope
   pure $ Stmt.Block resolved
+statement (Stmt.Variable name initializer) = do
+  declare name
+  resolved <- expression initializer
+  define name
+  pure $ Stmt.Variable name resolved
 statement stmt = pure stmt
+
+expression :: Expr.Expr -> Resolver Expr.Expr
+expression expr = pure expr
+
+declare :: Expr.Identifier -> Resolver ()
+declare name = do
+  s <- State.get
+  case s of
+    [] -> pure ()
+    (scope : scopes) -> State.put (Map.insert name False scope : scopes)
+
+define :: Expr.Identifier -> Resolver ()
+define name = do
+  s <- State.get
+  case s of
+    [] -> pure ()
+    (scope : scopes) -> State.put (Map.insert name True scope : scopes)
 
 beginScope :: Resolver ()
 beginScope = do
