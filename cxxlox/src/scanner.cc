10a0bca2a6901473bd3808b4e365288e14aac1ab
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-09-18 07:58:04 +0200

    [cxx] (16.3) scan symbolic tokens

--- cxxlox/src/scanner.cc
+++ cxxlox/src/scanner.cc
@@ -13,6 +13,23 @@ Scanner scanner;
 
 bool is_at_end() { return scanner.current == scanner.end; }
 
+char advance() {
+  auto const c = *scanner.current;
+  scanner.current++;
+  return c;
+}
+
+bool match(char expected) {
+  if (is_at_end()) {
+    return false;
+  }
+  if (*scanner.current != expected) {
+    return false;
+  }
+  scanner.current++;
+  return true;
+}
+
 Token make_token(TokenType type) {
   return {.type = type,
           .start = scanner.start,
@@ -41,6 +58,41 @@ Token scan_token() {
 
   if (is_at_end()) {
     return make_token(TokenType::END);
+  }
+
+  auto const c = advance();
+  switch (c) {
+  case '(':
+    return make_token(TokenType::LEFT_PAREN);
+  case ')':
+    return make_token(TokenType::RIGHT_PAREN);
+  case '{':
+    return make_token(TokenType::LEFT_BRACE);
+  case '}':
+    return make_token(TokenType::RIGHT_BRACE);
+  case ';':
+    return make_token(TokenType::SEMICOLON);
+  case ',':
+    return make_token(TokenType::COMMA);
+  case '.':
+    return make_token(TokenType::DOT);
+  case '-':
+    return make_token(TokenType::MINUS);
+  case '+':
+    return make_token(TokenType::PLUS);
+  case '/':
+    return make_token(TokenType::SLASH);
+  case '*':
+    return make_token(TokenType::STAR);
+  case '!':
+    return make_token(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG);
+  case '=':
+    return make_token(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL);
+  case '<':
+    return make_token(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS);
+  case '>':
+    return make_token(match('=') ? TokenType::GREATER_EQUAL
+                                 : TokenType::GREATER);
   }
 
   return error_token("Unexpected character.");
