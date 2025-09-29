85ac9896bf0039d3bb32ec1325ee9d7567bcd9b6
Raphael Meyer <raphael.akira.meyer@gmail.com>
2025-09-29 22:10:22 +0200

    [c] (17.6) stitch parts of Pratt parser together

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -165,18 +165,51 @@ static void unary() {
 }
 
 static ParseRule rules[] = {
-    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
-    [TOKEN_MINUS] = {unary, binary, PREC_TERM},
-    [TOKEN_PLUS] = {NULL, binary, PREC_TERM},
-    [TOKEN_SLASH] = {NULL, binary, PREC_FACTOR},
-    [TOKEN_STAR] = {NULL, binary, PREC_FACTOR},
-    [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
-    [TOKEN_EOF] = {NULL, NULL, PREC_NONE},
+    // clang-format off
+    [TOKEN_LEFT_PAREN]    = {grouping,  NULL,   PREC_NONE},
+    [TOKEN_RIGHT_PAREN]   = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_LEFT_BRACE]    = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_RIGHT_BRACE]   = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_COMMA]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_DOT]           = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_MINUS]         = {unary,     binary, PREC_TERM},
+    [TOKEN_PLUS]          = {NULL,      binary, PREC_TERM},
+    [TOKEN_SEMICOLON]     = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_SLASH]         = {NULL,      binary, PREC_FACTOR},
+    [TOKEN_STAR]          = {NULL,      binary, PREC_FACTOR},
+    [TOKEN_BANG]          = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_BANG_EQUAL]    = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_EQUAL]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_EQUAL_EQUAL]   = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_GREATER]       = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_GREATER_EQUAL] = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_LESS]          = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_LESS_EQUAL]    = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_IDENTIFIER]    = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_STRING]        = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_NUMBER]        = {number,    NULL,   PREC_NONE},
+    [TOKEN_AND]           = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_CLASS]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_ELSE]          = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_FALSE]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_FOR]           = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_FUN]           = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_IF]            = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_NIL]           = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_OR]            = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_PRINT]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_RETURN]        = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_SUPER]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_THIS]          = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_TRUE]          = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_VAR]           = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_WHILE]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_ERROR]         = {NULL,      NULL,   PREC_NONE},
+    [TOKEN_EOF]           = {NULL,      NULL,   PREC_NONE},
+    // clang-format on
 };
 
 static void parse_precedence(Precedence precedence) {
-  (void)precedence;
-
   advance();
   ParseFn prefix_rule = get_rule(parser.previous.type)->prefix;
   if (prefix_rule == NULL) {
@@ -185,6 +218,12 @@ static void parse_precedence(Precedence precedence) {
   }
 
   prefix_rule();
+
+  while (precedence <= get_rule(parser.current.type)->precedence) {
+    advance();
+    ParseFn infix_rule = get_rule(parser.previous.type)->infix;
+    infix_rule();
+  }
 }
 
 static ParseRule *get_rule(TokenType type) { return &rules[type]; }
